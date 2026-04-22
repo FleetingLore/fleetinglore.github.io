8d4ed47a42698c12f27335beb935cd40d98799b1
石山灰 <Macro__9@outlook.com>
2026-04-22 09:00:47 +0800

    update css

--- lore-pages-src/default/styles.js
+++ lore-pages-src/default/styles.js
@@ -1,11 +1,11 @@
 (function () {
     const link1 = document.createElement('link');
     link1.rel = 'stylesheet';
-    link1.href = 'https://fleetinglore.github.io/lore-pages-scripts/style.css';
+    link1.href = 'https://fleetinglore.github.io/lore-pages-src/style.css';
     document.head.appendChild(link1);
 
     const link2 = document.createElement('link');
     link2.rel = 'stylesheet';
-    link2.href = 'https://fleetinglore.github.io/lore-pages-scripts/foldable.css';
+    link2.href = 'https://fleetinglore.github.io/lore-pages-src/foldable.css';
     document.head.appendChild(link2);
 })();
